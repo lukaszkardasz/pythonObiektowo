a5440d9096e28164d69ea848e511ba2c1a2825fe
lkardasz <kardasz.lukasz@gmail.com>
2025-11-22 23:26:58 +0100

    Add initial implementation of Higher or Lower game logic

---
+++ HigherOrLowerProcedural.py
@@ -0,0 +1,16 @@
+#Higher or lower
+
+import random
+
+#Stałe przedstawiające karty
+SUIR_TUPLE = ('pik', 'karo', 'trefl', 'kier')
+RANK_TUPLE = ('as', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'walet', 'dama', 'król')
+
+NCARDS = 8
+
+#Przekazanie talii. Wartością zwrotną funkcji jest losowo wybrana karta z talii.
+
+def getCatd(deckListIN):
+    thisCard = deckListIn.pop() #Pobranie jednej karty z góry talii i jej zwrot
+    return thisCard
+
